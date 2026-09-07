e50ec9f144aec338a8485a159a3c44246398d31d
Serhat Bilgici <igesaco@gmail.com>
2026-09-07 15:48:06 +0300

    Provide roles guard for secured module

--- apps/api/src/customers/customers.module.ts
+++ apps/api/src/customers/customers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CustomersService } from './customers.service';
+
+import { RolesGuard } from '../auth/roles.guard';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
 
 @Module({
-  providers: [CustomersService],
-  controllers: [CustomersController]
+  controllers: [CustomersController],
+  providers: [CustomersService, RolesGuard],
 })
 export class CustomersModule {}
