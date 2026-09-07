1e6e29ee423a760222baf5e7ad3a5ebc7ea69faf
Serhat Bilgici <igesaco@gmail.com>
2026-09-07 16:02:56 +0300

    Provide roles guard for media module

--- apps/api/src/media/media.module.ts
+++ apps/api/src/media/media.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MediaService } from './media.service';
+
+import { RolesGuard } from '../auth/roles.guard';
 import { MediaController } from './media.controller';
+import { MediaService } from './media.service';
 
 @Module({
-  providers: [MediaService],
-  controllers: [MediaController]
+  providers: [
+    MediaService,
+    RolesGuard,
+  ],
+  controllers: [MediaController],
 })
 export class MediaModule {}
