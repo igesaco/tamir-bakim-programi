a19b93aca27c21a12850239f5f034502079318a0
Serhat Bilgici <igesaco@gmail.com>
2026-09-07 17:14:23 +0300

    Clean auth controller

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+
 import { AuthService } from './auth.service';
-import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('register')
   register(@Body() dto: RegisterDto) {
