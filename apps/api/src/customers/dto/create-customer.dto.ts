91247ad5937a91666df11a08e3686e43cb6f6896
Serhat Bilgici <igesaco@gmail.com>
2026-09-07 17:22:01 +0300

    Allow management branch selection when creating customers

--- apps/api/src/customers/dto/create-customer.dto.ts
+++ apps/api/src/customers/dto/create-customer.dto.ts
@@ -1,4 +1,9 @@
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateCustomerDto {
   @IsString()
@@ -16,6 +21,10 @@ export class CreateCustomerDto {
   @IsOptional()
   @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsString()
+  branchId?: string;
 
   @IsOptional()
   @IsString()
