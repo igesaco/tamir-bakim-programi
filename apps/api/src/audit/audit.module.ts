e2a3b58c3155b2f86ce9a0e1e335281658744080
Serhat Bilgici <igesaco@gmail.com>
2026-09-07 15:36:36 +0300

    Provide roles guard for audit module

--- apps/api/src/audit/audit.module.ts
+++ apps/api/src/audit/audit.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
+import { RolesGuard } from '../auth/roles.guard';
 import { AuditController } from './audit.controller';
-import { AuditService } from './audit.service';
 import { AuditInterceptor } from './audit.interceptor';
+import { AuditService } from './audit.service';
 
 @Module({
   controllers: [AuditController],
   providers: [
     AuditService,
+    RolesGuard,
     {
       provide: APP_INTERCEPTOR,
       useClass: AuditInterceptor,
