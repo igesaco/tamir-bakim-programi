311c65251af276280bacd02f2a48db4e1bf1306a
Serhat Bilgici <igesaco@gmail.com>
2026-09-07 17:12:36 +0300

    Add staff account security DTOs

---
+++ apps/api/src/users/dto/change-password.dto.ts
@@ -0,0 +1,14 @@
+import {
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class ChangePasswordDto {
+  @IsString()
+  @MinLength(6)
+  currentPassword: string;
+
+  @IsString()
+  @MinLength(8)
+  newPassword: string;
+}
