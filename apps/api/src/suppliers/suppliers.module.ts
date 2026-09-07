f5390f697205301046c1ebed79dcb211d4f38a7f
Serhat Bilgici <igesaco@gmail.com>
2026-09-07 15:36:48 +0300

    Provide roles guard for suppliers module

--- apps/api/src/suppliers/suppliers.module.ts
+++ apps/api/src/suppliers/suppliers.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { SuppliersService } from './suppliers.service';
+
+import { RolesGuard } from '../auth/roles.guard';
 import { SuppliersController } from './suppliers.controller';
+import { SuppliersService } from './suppliers.service';
 
 @Module({
-  providers: [SuppliersService],
-  controllers: [SuppliersController]
+  providers: [
+    SuppliersService,
+    RolesGuard,
+  ],
+  controllers: [SuppliersController],
 })
 export class SuppliersModule {}
