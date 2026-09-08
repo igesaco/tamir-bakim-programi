6e9e03da320c31440a3a631520bc2fee7196e208
Serhat Bilgici <igesaco@gmail.com>
2026-09-08 09:53:54 +0300

    Make entitlements available to authentication

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
+import { EntitlementsModule } from '../entitlements/entitlements.module';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -11,6 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
+    EntitlementsModule,
     UsersModule,
     PassportModule,
     JwtModule.register({
