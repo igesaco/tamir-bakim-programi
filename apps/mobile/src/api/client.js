ac87b0c6e6a0d38c9c23f1ea9a8f0c7a76ff2451
Serhat Bilgici <igesaco@gmail.com>
2026-09-08 16:00:42 +0300

    Point shared mobile app to live Render API by default

--- apps/mobile/src/api/client.js
+++ apps/mobile/src/api/client.js
@@ -1,20 +1,17 @@
 import axios from 'axios';
-import { Platform } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 
-const fallbackUrl =
-  Platform.OS === 'android'
-    ? 'http://10.0.2.2:3000'
-    : 'http://127.0.0.1:3000';
+const productionApiUrl =
+  'https://tamir-bakim-api.onrender.com';
 
 export const API_URL = (
   process.env.EXPO_PUBLIC_API_URL ||
-  fallbackUrl
+  productionApiUrl
 ).replace(/\/+$/, '');
 
 const api = axios.create({
   baseURL: API_URL,
-  timeout: 15000,
+  timeout: 30000,
 });
 
 api.interceptors.request.use(
